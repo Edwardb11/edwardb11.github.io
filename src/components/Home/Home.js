2ca0599a645cbbd0ee8bc116f75a43a286f949ff
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-04-25 15:51:43 -0400

    style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,6 +11,7 @@ import {
   Socials,
   Quote,
 } from "../styles/Home.styled";
+import "./style.css";
 import projectIcon from "../../assets/icons/project-icon.svg";
 import aboutIcon from "../../assets/icons/about-me.svg";
 import { IconContext } from "react-icons";
@@ -27,6 +28,19 @@ const Home = () => {
       <StyledHome id="home">
         <Name>Edward Brito</Name>
         <Title>Full-Stack Developer</Title>
+        <p>
+          I <span className="change-text"></span>{" "}
+          <span className="responsive">r</span>
+          <span className="responsive">e</span>
+          <span className="responsive">s</span>
+          <span className="responsive">p</span>
+          <span className="responsive">o</span>
+          <span className="responsive">n</span>
+          <span className="responsive">s</span>
+          <span className="responsive">i</span>
+          <span className="responsive">v</span>
+          <span className="responsive">e</span> websites.
+        </p>
         <StyledButtonsContainer>
           <StyledButton
             icon={projectIcon}
