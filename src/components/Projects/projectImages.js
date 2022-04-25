09c5a8224be609bbf383d40202135fe11b73080e
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-04-25 15:33:30 -0400

    change data in all app

--- src/components/Projects/projectImages.js
+++ src/components/Projects/projectImages.js
@@ -1,27 +1,23 @@
-import project1Laptop from "../../assets/projects/project-1-laptop.jpg";
-import project1Mobile from "../../assets/projects/project-1-mobile.jpg";
-import project2Laptop from "../../assets/projects/project-2-laptop.jpg";
-import project2Mobile from "../../assets/projects/project-2-mobile.jpg";
-import project3Laptop from "../../assets/projects/project-3-laptop.jpg";
-import project3Mobile from "../../assets/projects/project-3-mobile.jpg";
-import project4Laptop from "../../assets/projects/project-4-laptop.jpg";
-import project4Mobile from "../../assets/projects/project-4-mobile.jpg";
-import project5Laptop from "../../assets/projects/project-5-laptop.jpg";
-import project5Mobile from "../../assets/projects/project-5-mobile.jpg";
-import project6Laptop from "../../assets/projects/project-6-laptop.jpg";
-import project6Mobile from "../../assets/projects/project-6-mobile.jpg";
+import advice from "../../assets/projects/advice.png";
+import game from "../../assets/projects/game.png";
+import meanStack from "../../assets/projects/meanStack.png";
+import todolits from "../../assets/projects/todolist.png";
+import odontologica from "../../assets/projects/odontologica.png";
+import blog from "../../assets/projects/blog.png";
+import task from "../../assets/projects/task.png";
+import ticCaculator from "../../assets/projects/tic-calculator.jpg";
+import appPresupuesto from "../../assets/projects/app-presupuesto.png";
+
 
 export {
-  project1Laptop,
-  project1Mobile,
-  project2Laptop,
-  project2Mobile,
-  project3Laptop,
-  project3Mobile,
-  project4Laptop,
-  project4Mobile,
-  project5Laptop,
-  project5Mobile,
-  project6Laptop,
-  project6Mobile,
+  advice,
+  game,
+  meanStack,
+  todolits,
+  odontologica,
+  blog,
+  task,
+  appPresupuesto,
+  ticCaculator
+
 };
